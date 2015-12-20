2034a9d5a961753b4c53862177d9a963995399ba
Julian Toumey <jtoumey@gmail.com>
2015-12-20 10:40:26 -0600

    All: moved to implementation_2 folder

---
+++ artific_dif.f90
@@ -0,0 +1,31 @@
+!**************************************************************************!
+!                                                                          !
+!  Module:       ARTIFIC_DIF.F90                                           !
+!                                                                          !
+!  Programmer:   Julian M. Toumey                                          !
+!                Madison, WI                                               !
+!                                                                          !
+!  Date:         Decemver 2015                                             !
+!                                                                          !
+!  Language:     FORTRAN90                                                 !
+!                                                                          !
+!  Description:  This code solves source-free pure convection in a 2-D     !
+!                domain. The flow direction is 45* from the horizontal     !
+!                in order to maximize the artificial diffusion term for    !
+!                comparing discretization schemes.                         !
+!                                                                          !
+!                This program differs from ARTIFICDIF2D in that it treats  !
+!                boundary conditions differently and allows either upwind  !
+!                or central differencing for the convection term.          !
+!                                                                          !
+!**************************************************************************!
+PROGRAM ARTIFIC_DIF
+IMPLICIT NONE
+!
+integer IL,JL,ii,jj,kk,iter
+real dx,dy,xmax,ymax
+double precision, dimension(:), allocatable :: x,y
+
+
+call read_input(xmax,ymax,IL,JL)
+END
